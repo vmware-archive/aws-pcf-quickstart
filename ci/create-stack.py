924449909eb9dd2d8386252fa4ee38de80f8d6b1
Matt Cholick <cholick@gmail.com>
2017-05-16 10:51:24 -0700

    Create stack better

--- ci/create-stack.py
+++ ci/create-stack.py
@@ -1,7 +1,16 @@
-import jinja2
-import os
+import boto3
+import datetime
+import time
 import sys
-from subprocess import call
+import os
+
+
+def describe_stack_status(cloudformation_client, stack_id):
+    describe_response = cloudformation_client.describe_stacks(StackName=stack_id)
+    stack = describe_response.get("Stacks")[0]
+
+    return stack.get('StackStatus')
+
 
 password = os.environ['AWS_CF_PASSWORD']
 domain = os.environ['AWS_CF_DOMAIN']
@@ -9,28 +18,49 @@ hostedzoneid = os.environ['AWS_CF_HOSTEDZONEID']
 sslcertificatearn = os.environ['AWS_CF_SSLCERTIFICATEARN']
 natkeypair = os.environ['AWS_CF_NATKEYPAIR']
 pivnettoken = os.environ['AWS_CF_PIVNETTOKEN']
+aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+aws_region = os.environ['AWS_INTEGRATION_REGION']
 
-with open('ci/parameters.j2.json', 'r') as template_file:
-    template = jinja2.Template(template_file.read())
-    ctx = {
-        "password": password,
-        "domain": domain,
-        "hostedzoneid": hostedzoneid,
-        "sslcertificatearn": sslcertificatearn,
-        "natkeypair": natkeypair,
-        "pivnettoken": pivnettoken
-    }
-    rendered = template.render(ctx)
+parameters = [
+    {"ParameterKey": "06RdsPassword", "ParameterValue": password},
+    {"ParameterKey": "14HostedZoneId", "ParameterValue": hostedzoneid},
+    {"ParameterKey": "01NATKeyPair", "ParameterValue": natkeypair},
+    {"ParameterKey": "07SSLCertificateARN", "ParameterValue": sslcertificatearn},
+    {"ParameterKey": "16OpsManagerAdminPassword", "ParameterValue": password},
+    {"ParameterKey": "15Domain", "ParameterValue": domain},
+    {"ParameterKey": "10ElbPrefix", "ParameterValue": "my-pcf-elb"},
+    {"ParameterKey": "14PCFKeyPair", "ParameterValue": natkeypair},
+    {"ParameterKey": "05RdsUsername", "ParameterValue": "admin"},
+    {"ParameterKey": "13AdminEmail", "ParameterValue": "noreply@pivotal.io"},
+    {"ParameterKey": "12PivnetToken", "ParameterValue": pivnettoken}
+]
 
-    with open('parameters.json', 'w') as rendered_file:
-        rendered_file.write(rendered)
+client = boto3.client(
+    service_name='cloudformation', region_name=aws_region, aws_access_key_id=aws_access_key_id,
+    aws_secret_access_key=aws_secret_access_key
+)
 
-    cmd = """
-    aws cloudformation create-stack \
-              --stack-name pcf-int-`date +%s` \
-              --capabilities CAPABILITY_IAM \
-              --template-body file:///`pwd`/cloudformation/quickstart-template.yml \
-              --parameters file:///`pwd`/parameters.json
- """
+with open('./cloudformation/quickstart-template.yml', 'r') as template_file:
+    template = template_file.read()
 
-    sys.exit(call(cmd, shell=True))
+    stack_name = "pcf-int-{}".format(int(datetime.datetime.now().timestamp()))
+    create_response = client.create_stack(
+        StackName=stack_name,
+        TemplateBody=template,
+        Parameters=parameters,
+        Capabilities=[
+            'CAPABILITY_IAM',
+        ],
+    )
+    stack_id = create_response.get("StackId")
+
+    stack_status = describe_stack_status(client, stack_id)
+    while stack_status == 'CREATE_IN_PROGRESS':
+        time.sleep(300)
+        stack_status = describe_stack_status(client, stack_id)
+
+    print("Final status {}".format(stack_status))
+    if stack_status != "CREATE_COMPLETE":
+        print("Stack creation did not complete, exiting...")
+        sys.exit(1)
