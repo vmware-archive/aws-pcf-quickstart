3218e25003bf1b62d0c2e6100de39b680d33b48d
Matt Cholick <cholick@gmail.com>
2017-05-18 08:29:52 -0700

    Helper script to generate self signed cert

---
+++ scripts/gen_ssl_certs.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+ROOT_DOMAIN=$1
+
+SSL_FILE=sslconf-${ROOT_DOMAIN}.conf
+
+#Generate SSL Config with SANs
+if [ ! -f $SSL_FILE ]; then
+cat > $SSL_FILE <<EOF
+[req]
+distinguished_name = req_distinguished_name
+req_extensions = v3_req
+[req_distinguished_name]
+countryName_default = US
+stateOrProvinceName_default = CA
+localityName_default = SF
+organizationalUnitName_default = Pivotal
+[ v3_req ]
+# Extensions to add to a certificate request
+basicConstraints = CA:FALSE
+keyUsage = nonRepudiation, digitalSignature, keyEncipherment
+subjectAltName = @alt_names
+[alt_names]
+DNS.1 = *.${ROOT_DOMAIN}
+DNS.2 = *.sys.${ROOT_DOMAIN}
+DNS.3 = *.apps.${ROOT_DOMAIN}
+DNS.4 = *.login.${ROOT_DOMAIN}
+DNS.5 = *.uaa.${ROOT_DOMAIN}
+EOF
+fi
+
+openssl genrsa -out ${ROOT_DOMAIN}.key 2048
+openssl req -new -out ${ROOT_DOMAIN}.csr -subj "/CN=*.${ROOT_DOMAIN}/O=Pivotal/C=US" -key ${ROOT_DOMAIN}.key -config ${SSL_FILE}
+openssl req -text -noout -in ${ROOT_DOMAIN}.csr
+openssl x509 -req -days 3650 -in ${ROOT_DOMAIN}.csr -signkey ${ROOT_DOMAIN}.key -out ${ROOT_DOMAIN}.crt -extensions v3_req -extfile ${SSL_FILE}
+openssl x509 -in ${ROOT_DOMAIN}.crt -text -noout
