9d78d9b93338df82a40dd7fab474b92c4354c943
Jeenal Shah <jshah@pivotal.io>
2017-05-19 13:23:01 -0700

    use AMI and disable download tiles step

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,7 +9,7 @@ sudo pip3 install ./vendor/*
 python3 quickstart.py configure-opsman-auth
 python3 quickstart.py configure-opsman-director
 python3 quickstart.py apply-changes
-python3 quickstart.py download-tiles
-python3 quickstart.py upload-assets '/tmp'
+#python3 quickstart.py download-tiles
+python3 quickstart.py upload-assets '/home/ubuntu'
 python3 quickstart.py configure-ert
 python3 quickstart.py apply-changes
