863ed8ac9d297e859a8a6c925b401ac235b31632
Matt Cholick <cholick@gmail.com>
2017-06-22 13:45:03 -0700

    Oops.

--- lib/wait_for_dns.py
+++ lib/wait_for_dns.py
@@ -13,7 +13,7 @@ def check_dns(domain: str):
 
 def wait_for_dns(my_settings: settings.Settings):
     while True:
-        domain = "opsman.{}".format(my_settings.pcf_input_domain())
+        domain = "opsman.{}".format(my_settings.pcf_input_domain)
         print("Checking DNS entry {}".format(domain))
         ip = check_dns(domain)
         if ip == my_settings.pcf_pcfopsmanagerinstanceip:
