e3b6e79bb780e9b980ffb9ffaf945afe91a30360
Jeenal Shah <jshah@pivotal.io>
2017-08-16 11:15:17 -0700

    delete buckets on failure and explicit policy dependency

---
+++ ci/delete-buckets.py
@@ -0,0 +1,76 @@
+# aws-pcf-quickstart
+#
+# Copyright (c) 2017-Present Pivotal Software, Inc. All Rights Reserved.
+#
+# This program and the accompanying materials are made available under
+# the terms of the under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+
+import boto3
+import botocore
+import botocore.exceptions
+import os
+
+
+def delete_bucket(bucket_name: str, region: str, key: str, secret: str):
+    print("Deleting bucket {}".format(bucket_name))
+    s3_client = boto3.client(
+        service_name='s3',
+        region_name=region,
+        aws_access_key_id=key,
+        aws_secret_access_key=secret
+    )
+    try:
+        contents = s3_client.list_objects(Bucket=bucket_name).get('Contents')
+        while contents is not None:
+            delete_keys = [{'Key': o.get('Key')} for o in contents]
+            s3_client.delete_objects(Bucket=bucket_name, Delete={
+                'Objects': delete_keys
+            })
+            contents = s3_client.list_objects(Bucket=bucket_name).get('Contents')
+        s3_client.delete_bucket(Bucket=bucket_name)
+    except botocore.exceptions.ClientError as e:
+        error = e.response.get('Error')
+        if not error or error.get('Code') != 'NoSuchBucket':
+            raise e
+
+
+def main():
+    aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
+    aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
+    prefix = "pcf-int-"
+
+    s3_client = boto3.client(
+        service_name='s3',
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
+    response = s3_client.list_buckets()
+    buckets = response.get('Buckets')
+    if len(buckets) < 0:
+        print("No buckets in response")
+        print(response)
+        sys.exit(0)
+
+    print("Buckets: {}".format(buckets))
+    for bucket in buckets:
+        bucket_name = bucket.get('Name')
+        response = s3_client.get_bucket_location(Bucket=bucket_name)
+        bucket_region = response.get('LocationConstraint')
+        if bucket_name.startswith(prefix):
+            delete_bucket(bucket_name, bucket_region, aws_access_key_id, aws_secret_access_key)
+
+
+if __name__ == "__main__":
+    main()
--- ci/delete-stack.py
+++ ci/delete-stack.py
@@ -16,64 +16,14 @@
 # limitations under the License.
 
 import json
-import os
 import sys
 import time
 
 import boto3
-import botocore
-import botocore.exceptions
+import os
 
 aws_access_key_id = os.environ['AWS_ACCESS_KEY_ID']
 aws_secret_access_key = os.environ['AWS_SECRET_ACCESS_KEY']
-
-
-def get_bucket_names(stack_name: str):
-    parameter_store_keys = [
-        "PcfOpsManagerS3Bucket",
-        "PcfElasticRuntimeS3BuildpacksBucket",
-        "PcfElasticRuntimeS3DropletsBucket",
-        "PcfElasticRuntimeS3PackagesBucket",
-        "PcfElasticRuntimeS3ResourcesBucket"
-    ]
-
-    ssm_client = boto3.client(
-        service_name='ssm', region_name=aws_region, aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key
-    )
-
-    parameter_names = ["{}.{}".format(stack_name, p) for p in parameter_store_keys]
-
-    response = ssm_client.get_parameters(
-        Names=parameter_names,
-        WithDecryption=False
-    )
-
-    param_results = response.get("Parameters")
-
-    buckets = [p.get('Value') for p in param_results]
-
-    return buckets
-
-
-def delete_bucket(bucket_name: str):
-    s3_client = boto3.client(
-        service_name='s3', region_name=aws_region, aws_access_key_id=aws_access_key_id,
-        aws_secret_access_key=aws_secret_access_key
-    )
-    try:
-        contents = s3_client.list_objects_v2(Bucket=bucket_name).get('Contents')
-        while contents is not None:
-            delete_keys = [{'Key': o.get('Key')} for o in contents]
-            s3_client.delete_objects(Bucket=bucket_name, Delete={
-                'Objects': delete_keys
-            })
-            contents = s3_client.list_objects_v2(Bucket=bucket_name).get('Contents')
-        s3_client.delete_bucket(Bucket=bucket_name)
-    except botocore.exceptions.ClientError as e:
-        error = e.response.get('Error')
-        if not error or error.get('Code') != 'NoSuchBucket':
-            raise e
 
 
 def describe_stack_status(cloudformation_client, stack_id):
@@ -87,7 +37,6 @@ with open('../aws-pcf-concourse-state/stackid', 'r') as file:
     stack_metadata = json.loads(file.read())
     stack_id = stack_metadata['stack_id']
     aws_region = stack_metadata['region']
-print("Deleting buckets for stack {}".format(stack_id))
 
 cf_client = boto3.client(
     service_name='cloudformation', region_name=aws_region, aws_access_key_id=aws_access_key_id,
@@ -97,11 +46,6 @@ cf_client = boto3.client(
 response = cf_client.describe_stacks(StackName=stack_id)
 stacks = response.get('Stacks')
 stack_name = stacks[0].get('StackName')
-
-buckets = get_bucket_names(stack_name)
-print("Buckets {}".format(buckets))
-for bucket in buckets:
-    delete_bucket(bucket)
 
 print("Deleting stack {}".format(stack_id))
 cf_client.delete_stack(StackName=stack_id)
