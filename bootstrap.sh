c9f4336f7d7d53c9672eefe653ae2b7f5ed49cc4
ramonskie <ramonmakkelie@gmail.com>
2019-04-11 16:15:06 +0200

    use our go code for ci

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,11 @@ set -ex
 cd /home/ubuntu/quickstart
 
 export PATH=./bin:$PATH
-sudo pip3 install ./vendor/*
+# sudo pip3 install ./vendor/*
 
-python3 -u quickstart.py
+# python3 -u quickstart.py
+
+export GO111MODULE=on # manually active module mode
+export GOFLAGS=-mod=vendor
+go build .
+./aws-pcf-quickstart build
