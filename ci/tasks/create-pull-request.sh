61f3cfa0ce24b36ec171491a4c90335a2c27a9c5
ramonskie <ramonmakkelie@gmail.com>
2020-01-09 11:41:18 +0100

    use a simplyfied way of make a PR

---
+++ ci/tasks/create-pull-request.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+set -x
+
+export GITHUB_USER=${GITHUB_USER}
+export GITHUB_PASSWORD=${GITHUB_PASSWORD}
+
+version=$(cat version/version)
+
+
+pushd omg-src-in
+
+hub pull-request --base aws-quickstart/quickstart-pivotal-cloudfoundry:master --head cf-platform-eng/quickstart-pivotal-cloudfoundry:develop -m "Automated Pull Request from the starkandwayne CI - version ${version}"
+
+popd
