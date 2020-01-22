4bd2f1c37e408334c9128cbd547c9d0dd28fb0b5
ramonskie <ramonmakkelie@gmail.com>
2020-01-22 13:45:02 +0100

    put notes in temp output

--- ci/finalize-release.sh
+++ ci/finalize-release.sh
@@ -5,5 +5,5 @@ export IMAGE_VERSION=$(cat opsman-image/version)
 cp quickstart-scripts-alpha/quickstart.tgz output/quickstart-`cat version/version`.tgz
 
 spruce json templates/assets/deployment.yml \
-    | jq -r '.tiles | map("- \(.name)/\(.version) (\(.stemcell.product_slug | split("-") | .[-1])/\(.stemcell.release_version))") | .[]' > notes.md
-echo "- ops-manager/${IMAGE_VERSION}" >> notes.md
+    | jq -r '.tiles | map("- \(.name)/\(.version) (\(.stemcell.product_slug | split("-") | .[-1])/\(.stemcell.release_version))") | .[]' > output/notes.md
+echo "- ops-manager/${IMAGE_VERSION}" >> output/notes.md
