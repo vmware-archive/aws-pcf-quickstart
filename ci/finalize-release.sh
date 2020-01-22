ada6cf252746a7a2551aedb11140b2cfb024c908
ramonskie <ramonmakkelie@gmail.com>
2020-01-22 14:02:26 +0100

    location location location

--- ci/finalize-release.sh
+++ ci/finalize-release.sh
@@ -4,6 +4,6 @@ export IMAGE_VERSION=$(cat opsman-image/version)
 
 cp quickstart-scripts-alpha/quickstart.tgz output/quickstart-`cat version/version`.tgz
 
-spruce json templates/assets/deployment.yml \
+spruce json quickstart-repo/templates/assets/deployment.yml \
     | jq -r '.tiles | map("- \(.name)/\(.version) (\(.stemcell.product_slug | split("-") | .[-1])/\(.stemcell.release_version))") | .[]' > output/notes.md
 echo "- ops-manager/${IMAGE_VERSION}" >> output/notes.md
