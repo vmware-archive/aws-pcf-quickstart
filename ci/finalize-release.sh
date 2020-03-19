1ae4efda6a3cb45c64eca6c6dc15e9b2e546f1c8
ramonskie <ramonmakkelie@gmail.com>
2020-03-19 11:20:21 +0100

    add healthwatch to release notes

--- ci/finalize-release.sh
+++ ci/finalize-release.sh
@@ -7,6 +7,6 @@ export IMAGE_VERSION=$(cat opsman-tile/version)
 
 cp quickstart-scripts-alpha/quickstart.tgz output/quickstart-`cat version/version`.tgz
 
-spruce json quickstart-repo/templates/assets/deployment.yml \
+bosh int quickstart-repo/templates/assets/deployment.yml -o quickstart-repo/templates/assets/options/healthwatch.yml | spruce json \
     | jq -r '.tiles | map("- \(.name)/\(.version) (\(.stemcell.product_slug | split("-") | .[-1])/\(.stemcell.release_version))") | .[]' > output/notes.md
 echo "- ops-manager/${IMAGE_VERSION}" >> output/notes.md
